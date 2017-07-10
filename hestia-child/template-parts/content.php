b6a61aa0e48ca68e4e36f0c2138685eda822fe61
FordLambert <ford.lambert@protonmail.ch>
2017-07-10 14:25:43 +0200

    Added the child theme
    Just added the Hestia child theme, with modification for two column
    instead of three and a clickable button with it (bonus !), plus all the
    button on the main page now have the "btn btn-primary" class.

---
+++ hestia-child/template-parts/content.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for index/archive/search.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php ( is_sticky() && is_home() && ! is_paged() ) ? post_class( 'card card-raised card-blog' ): post_class( 'card card-plain card-blog' ); ?>>
+	<div class="row">
+		<?php
+		$post_thumbnail_url = get_the_post_thumbnail( get_the_ID(), 'hestia-blog' );
+		if ( ! empty( $post_thumbnail_url ) ) : ?>
+		<div class="col-md-5">
+			<div class="card-image">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<?php echo $post_thumbnail_url; ?>
+				</a>
+			</div>
+		</div>
+
+		<div class="col-md-7">
+			<?php else : ?>
+			<div class="col-md-12">
+				<?php endif; ?>
+				
+				<?php the_title( sprintf( '<h2 class="card-title"><a href="%s" title="%s" rel="bookmark">', esc_url( get_permalink() ), the_title_attribute( array(
+					'echo' => false,
+				) ) ), '</a></h2>' ); ?>
+				<div class="card-description">
+					<p>
+						<?php
+						the_meta();
+						$hestia_more = strpos( $post->post_content, '<!--more' );
+						if ( $hestia_more ) :
+							echo get_the_content();
+						else :
+							echo get_the_excerpt();
+						endif;
+						?>
+					</p>
+				</div>
+			</div>
+		</div>
+
+</article>
